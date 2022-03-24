8f1e2fae058f968aadee3b67bc8e026d36a7ffe2
abiari <abiari@student.1337.ma>
2022-03-24 11:50:37 +0100

    project done

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,75 @@
 <template>
   <div class="app">
-    <p> {{name}} </p>
-    <button @click="changeName('Zelda')">change name</button>
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="sortBy('title')">order by title</button>
+        <button @click="sortBy('salary')">order by salary</button>
+        <button @click="sortBy('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :orderTerm="orderTerm"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import JobList from './components/JobsList.vue'
+import Job from './types/Job'
+import OrderFilter from './types/OrderFilter'
 
 export default defineComponent({
   name: 'App',
-  components: {},
-  data() {
-    return {
-      name: 'Link'
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    const orderTerm = ref<OrderFilter>('title')
+
+    const sortBy = (filter: OrderFilter) => {
+      orderTerm.value = filter
     }
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name
-    }
+
+    return { jobs, sortBy, orderTerm }
   }
 });
 </script>
 
 <style>
-
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    color: #1195c9;
+    margin: 0 10px;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  header .title {
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
 </style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="job-list">
+		<p>Ordered by {{ orderTerm }} </p>
+		<transition-group name="list" tag="ul">
+			<li v-for="job in orderedJobs" :key="job.id">
+				<h2> {{ job.title }} in {{ job.location }} </h2>
+				<div class="salary">
+					<img src="../assets/rupee.svg" alt="currency">
+					<p>{{ job.salary }} rupees</p>
+				</div>
+				<div class="description">
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, veritatis ut similique voluptatem quae corporis ullam expedita dolore repudiandae. Consequuntur repudiandae similique, voluptatem consequatur aspernatur reiciendis quasi voluptates hic culpa?</p>
+				</div>
+			</li>
+		</transition-group>
+	</div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+import OrderFilter from '@/types/OrderFilter'
+ 
+ export default defineComponent({
+	props: {
+		jobs: {
+			required: true,
+			type: Array as PropType<Job[]>
+		},
+		orderTerm: {
+			required: true,
+			type: String as PropType<OrderFilter>
+		}
+	},
+	setup(props) {
+		const orderedJobs = computed(() => {
+			return [...props.jobs].sort((a: Job, b: Job) => {
+				return a[props.orderTerm] > b[props.orderTerm] ? 1 : -1
+			})
+		})
+
+		return { orderedJobs }
+	}
+})
+</script>
+
+<style scoped>
+	.job-list {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+	.job-list ul {
+		padding: 0;
+	}
+	.job-list li {
+		list-style-type: none;
+		background: white;
+		padding: 16px;
+		margin: 16px 0;
+		border-radius: 9px;
+	}
+	.job-list h2 {
+		margin: 0 0 10px;
+		text-transform: capitalize;
+	}
+	.salary {
+		display: flex;
+	}
+	.salary img {
+		width: 30px;
+	}
+	.salary p {
+		color: #17bf66;
+		font-weight: bold;
+		margin: 10px 4px;
+	}
+	.list-move {
+		transition: all 1s;
+	}
+</style>
